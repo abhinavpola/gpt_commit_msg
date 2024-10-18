9af30f2d75425c961efcadb53424539a856d4e72
Abhinav Pola <abhinavpola@google.com>
2024-10-18 14:54:25 -0700

    change defaults
    rm llmconfig

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="my_commit_hook",
+    name="commit_msg_hook",
     version="0.1",
     packages=find_packages(),
     include_package_data=True,
@@ -10,7 +10,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "install-commit-hook=my_commit_hook.install_hook:install_hook",
+            "install-commit-hook=commit_msg_hook.install_hook:install_hook",
         ],
     },
 )
